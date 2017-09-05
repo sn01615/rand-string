871ff33fd498287ee0e605dad3e2d4cc1e71be83
杨龙 <168119405@qq.com>
2017-09-05 18:35:17 +0800

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+# RandString
+
+## Install
+```
+composer require sn01615/rand-string
+```
+
+### Usage
+```php
+use PhpUtils\RandString;
+
+echo RandString::getRandString(32);
+```
---
+++ src/RandString.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace PhpUtils;
+
+class RandString
+{
+
+    static public function getRandString($length)
+    {
+        $str = null;
+        $strPol = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz";
+        $max = strlen($strPol) - 1;
+        for ($i = 0; $i < $length; $i++) {
+            $str .= $strPol[mt_rand(0, $max)];
+        }
+        return $str;
+    }
+}
+
